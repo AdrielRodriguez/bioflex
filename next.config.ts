8dd9e94dec37f64f866daf09305a33edd11a5010
Adriel Rodriguez <rodriguezadri500@gmail.com>
2025-11-18 15:55:24 -0300

    Completo

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbopack: {
-      root: process.cwd(),
-    },
-  },
+
 };
 
 export default nextConfig;
